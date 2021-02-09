2682ca88d3189694616a0b71b3a3cd84b1b300cb
Jorge Teixeira <jorgeteixe11@gmail.com>
2021-02-09 01:06:05 +0100

    add sieve of eratosthenes

---
+++ algebra/sieve_of_eratosthenes/README.md
@@ -0,0 +1,11 @@
+# Sieve of Eratosthenes
+## Definition
+![Definition](definition.png)
+#### LaTex code
+```latex
+a \in \mathbb{Z} \text{ with } a > 1\\
+\forall p \in \text{\{primes | p } \leq \sqrt a \text{\}:} \\
+p \nmid a \rightarrow a \in \text{\{primes\}}
+```
+## How it was implemented?
+Detailed explanation [here](https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes#Overview).
--- algebra/sieve_of_eratosthenes/sieve_of_eratosthenes.py
+++ algebra/sieve_of_eratosthenes/sieve_of_eratosthenes.py
@@ -1,6 +1,22 @@
+def sieve_of_eratosthenes(n):
+    if n < 1:
+        raise AttributeError('n has to be greater than 1')
+    is_prime = [True] * n
+    i = 2
+    while i * i < n:
+        if is_prime[i]:
+            j = i * i
+            while j < n:
+                is_prime[j] = False
+                j += i
+        i += 1
+    return [i for i in range(1, len(is_prime)) if is_prime[i]]
+
 
 def main():
-    pass
+    n = 100
+    primes = sieve_of_eratosthenes(n)
+    print(f'primes = {primes}')
 
 
 if __name__ == '__main__':
